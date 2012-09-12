f8fad9a2281966b620f6d03ebcab9c68419c771c
Andy Shinn <andys@andyshinn.as>
2012-09-12 16:09:52 -0700

    preliminary support for EL systems, add update commands, and change config file to be OS specific

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,14 @@ class timezone::params {
     /(Ubuntu|Debian)/: {
       $package = 'tzdata'
       $zoneinfo_dir = '/usr/share/zoneinfo/'
-      $config_file = '/etc/localtime'
+      $config_file = '/etc/timezone'
+      $update_command = 'dpkg-reconfigure -f noninteractive tzdata'
+    }
+    /(RedHat|CentOS)/: {
+      $package = 'tzdata'
+      $zoneinfo_dir = '/usr/share/zoneinfo/'
+      $config_file = '/etc/sysconfig/clock'
+      $update_command = 'tzdata-update'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
